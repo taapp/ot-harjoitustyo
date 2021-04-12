31e8f4dc1ac6689957c8b74d1deaff9497d38417
taapp <julius.nieminen@helsinki.fi>
2021-04-12 19:32:35 +0300

    Luotu testaukseen ja testikattavuuteen liittyvät perustoiminnot

--- tasks.py
+++ tasks.py
@@ -3,3 +3,16 @@ from invoke import task
 @task
 def start(ctx):
     ctx.run("python3 src/index.py")
+
+
+@task
+def test(ctx):
+    ctx.run("pytest src")
+
+@task
+def coverage(ctx):
+    ctx.run("coverage run --branch -m pytest src")
+
+@task(coverage)
+def coverage_report(ctx):
+    ctx.run("coverage html")
