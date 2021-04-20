3a1b4fb22028d2819e179ec424c5efaf51eea352
taapp <julius.nieminen@helsinki.fi>
2021-04-20 21:59:10 +0300

    Päivitetty database_connection.py

--- src/database_connection.py
+++ src/database_connection.py
@@ -1,7 +1,7 @@
 import sqlite3
 from pathlib import Path
 
-DATABASE_FILE_PATH = Path(__file__).parent / "data" / "database.db"
+DATABASE_FILE_PATH = str(Path(__file__).parent / "data" / "database.db")
 CONNECTION = sqlite3.connect(DATABASE_FILE_PATH)
 CONNECTION.row_factory = sqlite3.Row
 
