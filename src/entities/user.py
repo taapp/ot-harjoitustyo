34475776d36d0b64382a2639b9ae546caf4cac56
taapp <julius.nieminen@helsinki.fi>
2021-04-20 08:14:49 +0300

    Päivitetty user.py

--- src/entities/user.py
+++ src/entities/user.py
@@ -1,8 +1,8 @@
 class User:
-    def __init__(self, id_user, name, type_user):
+    def __init__(self, id_user, name, admin):
         self.id = id_user
         self.name = name
-        self.type = type_user
+        self.admin = admin
 
     def __str__(self):
-        return f"User, name: {self.name}, type: {self.type}"
+        return f"User, name: {self.name}, type: {self.admin}"
