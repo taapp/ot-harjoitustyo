dc7e25d8ae02740a183bd0b7589d4252cbf9e65f
taapp <julius.nieminen@helsinki.fi>
2021-05-04 20:54:06 +0300

    Luotu docstringit user.py:hyn

--- src/entities/user.py
+++ src/entities/user.py
@@ -1,9 +1,31 @@
 class User:
+    """Luokka, joka vastaa sovelluksen käyttäjää.
+
+    Attributes:
+        id_user: Merkkijonoarvoa, joka vastaa käyttäjän tunnistetta.
+        name: Merkkijono, joka vastaa käyttäjänimeä.
+        password: Merkkijono, joka vastaa käyttäjän salasanaa.
+        admin: Boolean-arvo, joka kertoo, onko käyttäjä pääkäyttäjä.
+    """
+
     def __init__(self, id_user, name, password, admin):
+        """Luokan konstruktori, joka luo uuden käyttäjän.
+
+        Args:
+            id_user: Merkkijonoarvoa, joka vastaa käyttäjän tunnistetta.
+            name: Merkkijono, joka vastaa käyttäjänimeä.
+            password: Merkkijono, joka vastaa käyttäjän salasanaa.
+            admin: Boolean-arvo, joka kertoo, onko käyttäjä pääkäyttäjä.
+        """
         self.id = id_user
         self.name = name
         self.password = password
         self.admin = admin
 
     def __str__(self):
+        """Palauttaa käyttäjäolion merkkijonoesityksen.
+
+        Returns:
+            Merkkijono, joka sisältää käyttäjän perustiedot.
+        """
         return f"User, name: {self.name}, type: {self.admin}"
