0d4f48ba13bd9e5739490732046c46e107640b9a
taapp <julius.nieminen@helsinki.fi>
2021-05-16 19:46:29 +0300

    Lisätty testejä question_service_test.py:hyn

--- src/tests/services/question_service_test.py
+++ src/tests/services/question_service_test.py
@@ -23,3 +23,43 @@ class TestQuestionService(unittest.TestCase):
     def test_create_user(self):
         user = question_service.create_user('Kalle', 'passu', False)
         self.assertEqual(user.__str__(), "User, name: Kalle, type: False")
+
+    def test_load_series_by_name(self):
+        question_service.load_series_by_name("default")
+        self.assertEqual(question_service.cur_series.name, "default")
+
+    def test_load_all_questions(self):
+        question_service.load_all_questions()
+        self.assertEqual(len(question_service.list_questions), 3)
+
+    def test_load_user(self):
+        user_unknown = question_service.load_user('tuntematon', 'passut')
+        self.assertEqual(user_unknown, None)
+
+    def test_save_user(self):
+        res = question_service.save_user('Kalle', 'passu', False)
+        self.assertEqual(res, True)
+        res_2 = question_service.save_user('Kalle', 'passu', False)
+        self.assertEqual(res_2, False)
+
+    def test_set_current_user(self):
+        user = question_service.create_user('Kalle', 'passu', False)
+        question_service.set_current_user(user)
+        self.assertEqual(question_service.cur_user.password, "passu")
+
+    def test_current_user_is_admin(self):
+        res_no_user = question_service.current_user_is_admin()
+        self.assertEqual(res_no_user, None)
+        user = question_service.create_user('Kalle', 'passu', False)
+        question_service.set_current_user(user)
+        res_not_admin = question_service.current_user_is_admin()
+        self.assertEqual(res_not_admin, False)
+        user_admin = question_service.create_user('Ville', 'salsasana', True)
+        question_service.set_current_user(user_admin)
+        res_admin = question_service.current_user_is_admin()
+        self.assertEqual(res_admin, True)
+
+    def test_create_uuid(self):
+        uuid_new = question_service.create_uuid()
+        self.assertEqual(len(uuid_new), 36)
+        self.assertEqual(type(uuid_new), str)
