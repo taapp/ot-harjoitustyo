e197a2cf2d6682b3cedeb51da110104ae31a27a3
taapp <julius.nieminen@helsinki.fi>
2021-05-16 10:06:59 +0300

    AdminView:lle luotu oma tiedosto

---
+++ src/ui/admin_view.py
@@ -0,0 +1,61 @@
+
+from tkinter import ttk, constants
+
+
+class AdminView:
+    def __init__(self, root, button_handler_create_series, button_handler_series_list, button_handler_logout):
+        self._root = root
+        #self._question_text = question_text
+        self._frame = None
+        self._check_admin = None
+        self._button_handler_create_series = button_handler_create_series
+        self._button_handler_series_list = button_handler_series_list
+        self._button_handler_logout = button_handler_logout
+
+        self._initialize()
+
+    def pack(self):
+        self._frame.pack(fill=constants.X)
+
+    def destroy(self):
+        self._frame.destroy()
+
+    def _initialize(self):
+        self._frame = ttk.Frame(master=self._root)
+        label_welcome = ttk.Label(
+            master=self._frame, text="Welcome.")
+
+        button_create_user = ttk.Button(
+            master=self._frame,
+            text="Create a quiz",
+            command=self._handle_button_click_create_series
+        )
+
+        button_series_list = ttk.Button(
+            master=self._frame,
+            text="Take a quiz",
+            command=self._handle_button_click_series_list
+        )
+
+        button_logout = ttk.Button(
+            master=self._frame,
+            text="Logout",
+            command=self._handle_button_click_logout
+        )
+
+        #label.grid(row=0, column=0)
+        #button.grid(row=1, column=0)
+        label_welcome.grid()
+        button_create_user.grid()
+        button_series_list.grid()
+        button_logout.grid()
+
+    def _handle_button_click_create_series(self):
+        print("Adminview, _handle_button_click_create_series")
+        self._button_handler_create_series()
+
+    def _handle_button_click_series_list(self):
+        self._button_handler_series_list()
+
+    def _handle_button_click_logout(self):
+        self._button_handler_logout()
--- src/ui/ui.py
+++ src/ui/ui.py
@@ -4,6 +4,7 @@ from functools import partial
 from ui.report_view import ReportView
 from ui.create_question_view import CreateQuestionView
 from ui.create_series_view import CreateSeriesView
+from ui.admin_view import AdminView
 
 
 class Scrollable(ttk.Frame):
@@ -515,62 +516,3 @@ class UI:
     def _handle_end_series_button(self):
         question_service.empty_current_series()
         self._handle_login_button()
-
-
-class AdminView:
-    def __init__(self, root, button_handler_create_series, button_handler_series_list, button_handler_logout):
-        self._root = root
-        #self._question_text = question_text
-        self._frame = None
-        self._check_admin = None
-        self._button_handler_create_series = button_handler_create_series
-        self._button_handler_series_list = button_handler_series_list
-        self._button_handler_logout = button_handler_logout
-
-        self._initialize()
-
-    def pack(self):
-        self._frame.pack(fill=constants.X)
-
-    def destroy(self):
-        self._frame.destroy()
-
-    def _initialize(self):
-        self._frame = ttk.Frame(master=self._root)
-        label_welcome = ttk.Label(
-            master=self._frame, text="Welcome.")
-
-        button_create_user = ttk.Button(
-            master=self._frame,
-            text="Create a quiz",
-            command=self._handle_button_click_create_series
-        )
-
-        button_series_list = ttk.Button(
-            master=self._frame,
-            text="Take a quiz",
-            command=self._handle_button_click_series_list
-        )
-
-        button_logout = ttk.Button(
-            master=self._frame,
-            text="Logout",
-            command=self._handle_button_click_logout
-        )
-
-        #label.grid(row=0, column=0)
-        #button.grid(row=1, column=0)
-        label_welcome.grid()
-        button_create_user.grid()
-        button_series_list.grid()
-        button_logout.grid()
-
-    def _handle_button_click_create_series(self):
-        print("Adminview, _handle_button_click_create_series")
-        self._button_handler_create_series()
-
-    def _handle_button_click_series_list(self):
-        self._button_handler_series_list()
-
-    def _handle_button_click_logout(self):
-        self._button_handler_logout()
