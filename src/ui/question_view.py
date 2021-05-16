dc300e8ac14efb1d090fd4af804912a53424618d
taapp <julius.nieminen@helsinki.fi>
2021-05-16 10:14:23 +0300

    QuestionView:lle luotu oma tiedosto

---
+++ src/ui/question_view.py
@@ -0,0 +1,54 @@
+from tkinter import ttk, constants, StringVar
+from services.question_service import question_service
+
+
+class QuestionView:
+    def __init__(self, root, question_text, button_handler):
+        self._root = root
+        self._question_text = question_text
+        self._frame = None
+        self._entry = None
+        self._button_handler = button_handler
+
+        self._initialize()
+
+    def pack(self):
+        self._frame.pack(fill=constants.X)
+
+    def destroy(self):
+        self._frame.destroy()
+
+    def _initialize(self):
+        self._frame = ttk.Frame(master=self._root)
+        label = ttk.Label(master=self._frame, text=f"{self._question_text}")
+        self._entry = ttk.Entry(master=self._frame)
+        button = ttk.Button(
+            master=self._frame,
+            text="Give answer",
+            command=self._handle_button_click
+        )
+        self._error_text_var = StringVar()
+        self._error_text_var.set("")
+        label_error = ttk.Label(
+            master=self._frame, textvariable=self._error_text_var)
+        #label.grid(row=0, column=0)
+        #button.grid(row=1, column=0)
+        label.grid()
+        self._entry.grid()
+        label_error.grid()
+        button.grid()
+
+    def _handle_button_click(self):
+        entry_val = self._entry.get()
+        try:
+            entry_val = float(entry_val)
+        except ValueError:
+            self._error_text_var.set("The probability has to be a real number")
+            self.pack()
+            return
+        if entry_val < 0 or entry_val > 1:
+            self._error_text_var.set("The probability has to be in [0,1].")
+            self.pack()
+            return
+        question_service.give_new_answer(entry_val)
+        self._button_handler()
--- src/ui/ui.py
+++ src/ui/ui.py
@@ -6,6 +6,7 @@ from ui.create_question_view import CreateQuestionView
 from ui.create_series_view import CreateSeriesView
 from ui.admin_view import AdminView
 from ui.login_view import LoginView
+from ui.question_view import QuestionView
 
 
 class Scrollable(ttk.Frame):
@@ -71,59 +72,6 @@ class StartSeriesView:
         )
         button.grid()
 
-
-class QuestionView:
-    def __init__(self, root, question_text, button_handler):
-        self._root = root
-        self._question_text = question_text
-        self._frame = None
-        self._entry = None
-        self._button_handler = button_handler
-
-        self._initialize()
-
-    def pack(self):
-        self._frame.pack(fill=constants.X)
-
-    def destroy(self):
-        self._frame.destroy()
-
-    def _initialize(self):
-        self._frame = ttk.Frame(master=self._root)
-        label = ttk.Label(master=self._frame, text=f"{self._question_text}")
-        self._entry = ttk.Entry(master=self._frame)
-        button = ttk.Button(
-            master=self._frame,
-            text="Give answer",
-            command=self._handle_button_click
-        )
-        self._error_text_var = StringVar()
-        self._error_text_var.set("")
-        label_error = ttk.Label(
-            master=self._frame, textvariable=self._error_text_var)
-        #label.grid(row=0, column=0)
-        #button.grid(row=1, column=0)
-        label.grid()
-        self._entry.grid()
-        label_error.grid()
-        button.grid()
-
-    def _handle_button_click(self):
-        entry_val = self._entry.get()
-        try:
-            entry_val = float(entry_val)
-        except ValueError:
-            self._error_text_var.set("The probability has to be a real number")
-            self.pack()
-            return
-        if entry_val < 0 or entry_val > 1:
-            self._error_text_var.set("The probability has to be in [0,1].")
-            self.pack()
-            return
-        question_service.give_new_answer(entry_val)
-        self._button_handler()
-
-
 # class CreateUserView:
 #     def __init__(self, root, button_handler):
 #         self._root = root
@@ -181,6 +129,7 @@ class QuestionView:
 #         question_service.save_user(
 #             entry_username, entry_password, bool(is_admin))
 #         self._button_handler()
+
 
 class SeriesListView:
     def __init__(self, root, button_handler_take_quiz):
